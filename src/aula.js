750c998f2f596c52e43738524f059b5c09e18a0d
Lucas Oliveira <lucas.gon.oliv@gmail.com>
2023-10-24 10:37:01 -0300

    feito aula invertida

---
+++ src/aula.js
@@ -0,0 +1,39 @@
+// Importações necessárias.
+import logo from './logo.svg';           // Importa a imagem do logo.
+import './aula-invertida.css';           // Importa o CSS para estilização.
+import { useState } from 'react';        // Importa o hook useState do React.
+
+function AulaInvertida() {
+  
+  // Objeto onde você armazena os valores das propriedades que pertencem ao componente.
+
+  // Capturar os valores retornados por useState.
+  // Inicialização do estado "contagem" com valor 0.
+  const [contagem, setContagem] = useState(0);
+
+  //Contagem: Variável que armazena o valor atual do estado. Neste caso, é inicializado com o valor 0.
+  //setContagem: É a função que você usa para atualizar o valor de contagem. Quando você chama setContagem, o componente é renderizado com o novo valor do estado.
+
+  // Função para incrementar o valor de "contagem".
+  const incrementar = () => {
+    setContagem(contagem + 1);
+  };
+
+  // Renderização do componente.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        {/* Exibe o valor atual de "contagem". */}
+        <p className='contagem'>Contagem: {contagem}</p>
+        
+        {/* Botão que, ao ser clicado, chama a função "incrementar" para aumentar a contagem. */}
+        <button onClick={incrementar}>Incrementar</button>
+      </header>
+    </div>
+  );
+}
+
+// Exporta o componente para que possa ser usado em outras partes da aplicação.
+export default AulaInvertida;
